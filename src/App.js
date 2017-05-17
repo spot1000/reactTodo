e08df7618f094e8cf43e1222356cc683d3f75704
Adam Lichter <adamlichter@gmail.com>
2017-05-16 22:15:18 -0230

    can add to-dos

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TodoForm, TodoList} from './components/todo/';
+import{addTodo, generateId} from './lib/todoHelpers'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {id:1, name:'Learn JSX', isComplete: true},
+        {id:2, name:'Build App', isComplete: false},
+        {id:3, name:'ShipIt!', isComplete: false}
+      ],
+      currentTodo: '',
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleEmptySubmit = this.handleEmptySubmit.bind(this)
+  }
+
+  handleSubmit (evt) {
+    evt.preventDefault()
+    const newId = generateId()
+    const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
+    const updatedTodos = addTodo(this.state.todos, newTodo)
+    this.setState({
+      todos:updatedTodos,
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit (evt) {
+    evt.preventDefault()
+    this.setState({
+      errorMessage: 'Please supply a todo Name!'
+    })
+  }
+
+
+  handleInputChange(evt) {
+    this.setState({
+      currentTodo: evt.target.value
+    })
+  }
+
+  render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit: this.handleEmptySubmit
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React to-dos</h2>
+        </div>
+        <div className='Todo-App'>
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
+        <TodoForm handleSubmit={submitHandler}
+          handleInputChange={this.handleInputChange}
+          currentTodo={this.state.currentTodo}/>
+        <TodoList todos={this.state.todos}/>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
